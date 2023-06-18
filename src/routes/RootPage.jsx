3e5304b56e3dd525927f8f25070bb3a24ae4207e
blazejpak <blazej.pak@gmail.com>
2023-06-18 13:31:00 +0200

    change

--- src/routes/RootPage.jsx
+++ src/routes/RootPage.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import MainNavigation from "../components/mainNavigation";
+import MainNavigation from "../components/MainNavigation";
 
 const RootPage = () => {
   return (
